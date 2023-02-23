ebac121ddb213986ecefa69a45f0110455cf077e
Caleb Wise <caleb.wise@tlgcohort.com>
2023-02-23 12:43:33 -0800

    Lab 10-Extra Complete

---
+++ Lab10-Extra/src/com/hr/Employee.java
@@ -0,0 +1,108 @@
+package com.hr;
+
+import com.transportation.Car;
+import com.transportation.DestinationUnreachableException;
+
+public class Employee {
+    // INSTANCE VARIABLES
+    private String name;
+
+    // CONSTRUCTORS
+    public Employee(String name) {
+        this.name = name;
+    }
+
+    // BUSINESS METHODS
+
+    /*
+     * OPTION 4: try-catch, but then throw a different one.
+     * We are "nesting" the underlying 'cause' exception into a higher-level,
+     * more HR-oriented exception, for our Hr-oriented client.
+     */
+
+    public void goToWork() throws WorkException {
+        Car c = new Car("CAU0612", "Subaru", "Impreza");
+        c.start();
+
+        try {
+            c.moveTo("West Seattle");
+        }
+        catch (DestinationUnreachableException e){
+            throw new WorkException("Unable to get to work", e);
+        }
+        finally {
+            c.stop();
+        }
+    }
+
+    /*
+     * OPTION 3: try-catch, "respond" in some way, and then re-throw.
+     * "catch-and-rethrow"
+     */
+
+    /*
+    public void goToWork() throws DestinationUnreachableException {
+        Car c = new Car("CAU0612", "Subaru", "Impreza");
+        c.start();
+
+        try {
+            c.moveTo("West Seattle");
+        }
+        catch (DestinationUnreachableException e){
+            System.out.println("Email sent to Lisa Herbold to complain.");
+            throw e;
+        }
+        finally {
+            c.stop();
+        }
+    }
+    */
+
+    /*
+     * OPTION 2: "punt" i.e., don't handle any thrown exception at all.
+     * Because the exception in question is CHECKED, I MUST say "throws."
+     * NOTE: that a try-finally can be used for the regular finally reasons.
+     */
+    /*public void goToWork() throws DestinationUnreachableException {
+        Car c = new Car("CAU0612", "Subaru", "Impreza");
+        c.start();
+
+        try {
+            c.moveTo("West Seattle");
+        }
+        finally {
+            c.stop();
+        }
+    }
+    */
+
+    /*
+     * OPTION 1: try-catch-finally and handle any thrown exception.
+     */
+    /*
+    public void goToWork() {
+        Car c = new Car("CAU0612", "Subaru", "Impreza");
+
+        try {
+            c.start();
+            c.moveTo("Tacoma");
+        }
+        catch (DestinationUnreachableException e) {
+            System.out.println(e.getMessage());
+        }
+        finally {
+            c.stop();
+        }
+    }
+    */
+
+    // ACCESSOR METHODS
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return getClass().getSimpleName() + ": name=" + getName();
+    }
+}
